db1aed50d3363f7ea4e5807faf5dc905f89899cd
ewooten7 <111931887+ewooten7@users.noreply.github.com>
2024-08-06 19:50:15 -0400

    String Practice activities
    working through self-made practice problems using strings

---
+++ StringsPractice.sql
@@ -0,0 +1,15 @@
+﻿--Question 1. Add the letter A to the end of each name. (Use + to join)
+select [name] + 'A'
+from sys.all_columns
+
+--Question 2. Add the letter Ⱥ to the end of each name. (VARCHAR. Add N')
+select [name] + N'Ⱥ'
+from sys.all_columns
+
+--Question 3. Remove the first character from name (use SUBSTRING)
+select substring([name],2,len([name])-1) as [name]
+from sys.all_columns
+
+--Question 4. Remove the last original character from name (use LEN)
+select substring([name],1,len([name])-1) as [name]
+from sys.all_columns
