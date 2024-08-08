35695dee76907c97885c65000b256f4715fde812
ewooten7 <111931887+ewooten7@users.noreply.github.com>
2024-08-07 21:58:33 -0400

    Work on Employee listing database: editing

---
+++ TablePractice_CreateAndQuery.sql
@@ -0,0 +1,33 @@
+--ALTER TABLE tblEmployee2
+--ADD Department VARCHAR(10);
+
+--SELECT * FROM tblEmployee2
+
+--INSERT INTO tblEmployee2
+--VALUES (132, 'Dylan', 'A', 'Word', 'MO630170X', '19940205',	'Customer Relations')
+
+--ALTER TABLE tblEmployee2
+--DROP COLUMN Department
+
+--ALTER TABLE tblEmployee2
+--ADD Department VARCHAR(15)
+
+--ALTER TABLE tblEmployee2
+--ALTER COLUMN Department VARCHAR(20) NULL
+--go
+
+--NEW! Updating Column to accept NULLs, then coding solution for NULLs to be added to blanks
+ALTER TABLE tblEmployee2
+ALTER COLUMN EmployeeMiddleName VARCHAR(50) NULL;
+
+UPDATE tblEmployee2
+SET EmployeeMiddleName = NULL
+WHERE EmployeeMiddleName = '';
+
+
+
+--INSERT INTO tblEmployee2([EmployeeNumber],[EmployeeFirstName],
+--VALUES (132, 'Dylan', 'A', 'Word', 'MO630170X', '19940205',	'Customer Relations')
+
+SELECT * FROM tblEmployee2
+WHERE [EmployeeLastName] <> 'Word'
